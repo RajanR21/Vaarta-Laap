083b7897d3a42519d9c827471421da73a4e7dd7a
Rajan Ravisaheb <rajanroy220@gmail.com>
2024-07-11 19:57:10 +0530

    added pages for chatlists, notifications ,search user and others

---
+++ client/src/components/specific/Search.jsx
@@ -0,0 +1,55 @@
+import { useInputValidation } from "6pp";
+import { Search as SearchIcon } from "@mui/icons-material";
+import {
+  Dialog,
+  DialogTitle,
+  InputAdornment,
+  List,
+  Stack,
+  TextField,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import UserItem from "../shared/UserItem";
+
+const Search = ({ isSearch, searchCloseHandler }) => {
+  const search = useInputValidation("");
+  const users = [1, 2, 3];
+
+  const addFriendHandler = (id) => {};
+  const isLoadingSendFriendRequest = () => {};
+
+  return (
+    <Dialog open={isSearch} onClose={searchCloseHandler}>
+      <Stack p={"2rem"} direction={"column"} width={"25rem"}>
+        <DialogTitle textAlign={"center"}>Find People</DialogTitle>
+        <TextField
+          label=""
+          value={search.value}
+          onChange={search.changeHandler}
+          variant="outlined"
+          size="small"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+
+        <List>
+          {users?.map((i) => (
+            <UserItem
+              user={i}
+              key={i._id}
+              handler={addFriendHandler}
+              handlerIsLoading={isLoadingSendFriendRequest}
+            />
+          ))}
+        </List>
+      </Stack>
+    </Dialog>
+  );
+};
+
+export default Search;
