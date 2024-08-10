155fe8c9da8ca7d531f07e8f996712f206c24db0
Rajan Ravisaheb <rajanroy220@gmail.com>
2024-08-10 17:18:02 +0530

    main files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,58 @@
-import React, { lazy } from "react";
+import React, { Suspense, lazy, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProtectRoute from "./components/auth/ProtectRoute";
+import { LayoutLoader } from "./components/layout/Loaders";
+import axios from "axios";
+import { server } from "./constants/config";
+import { useDispatch, useSelector } from "react-redux";
+import { userExists, userNotExists } from "./redux/reducers/auth";
+import { Toaster } from "react-hot-toast";
+import { SocketProvider } from "./socket.jsx";
 
 const Home = lazy(() => import("./pages/Home"));
 const Login = lazy(() => import("./pages/Login"));
 const Chat = lazy(() => import("./pages/Chat"));
 const Groups = lazy(() => import("./pages/Groups"));
-// const NotFound = lazy(() => import("./pages/NotFound"));
+const NotFound = lazy(() => import("./pages/NotFound.jsx/index.js"));
+
+// const AdminLogin = lazy(() => import("./pages/admin/AdminLogin"));
+// const Dashboard = lazy(() => import("./pages/admin/Dashboard"));
+// const UserManagement = lazy(() => import("./pages/admin/UserManagement"));
+// const ChatManagement = lazy(() => import("./pages/admin/ChatManagement"));
+// const MessagesManagement = lazy(() =>
+//   import("./pages/admin/MessageManagement")
+// );
 
 const App = () => {
-  const user = true;
-  return (
-    <div>
-      <BrowserRouter>
+  const { user, loader } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    axios
+      .get(`${server}/user/me`, { withCredentials: true })
+      .then(({ data }) => dispatch(userExists(data.user)))
+      .catch((err) => dispatch(userNotExists()));
+  }, [dispatch]);
+
+  return loader ? (
+    <LayoutLoader />
+  ) : (
+    <BrowserRouter>
+      <Suspense fallback={<LayoutLoader />}>
         <Routes>
           <Route
-            path="/"
             element={
-              <ProtectRoute user={user} redirect="/login">
-                <Home />
-              </ProtectRoute>
+              <SocketProvider>
+                <ProtectRoute user={user} />
+              </SocketProvider>
             }
           >
-            <Route
-              path="/chat/:chatId"
-              element={
-                <ProtectRoute user={user} redirect="/login">
-                  <Chat />
-                </ProtectRoute>
-              }
-            />
-            <Route
-              path="/groups"
-              element={
-                <ProtectRoute user={user} redirect="/login">
-                  <Groups />
-                </ProtectRoute>
-              }
-            />
+            <Route path="/" element={<Home />} />
+            <Route path="/chat/:chatId" element={<Chat />} />
+            <Route path="/groups" element={<Groups />} />
           </Route>
+
           <Route
             path="/login"
             element={
@@ -47,9 +61,19 @@ const App = () => {
               </ProtectRoute>
             }
           />
+          {/* 
+          <Route path="/admin" element={<AdminLogin />} />
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/users" element={<UserManagement />} />
+          <Route path="/admin/chats" element={<ChatManagement />} />
+          <Route path="/admin/messages" element={<MessagesManagement />} /> */}
+
+          <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
-    </div>
+      </Suspense>
+
+      <Toaster position="bottom-center" />
+    </BrowserRouter>
   );
 };
 
